e7e8686ef2b334c97ebbeffa0a6d3f5891f1fa1d
XIANQw <qiwei-xian@qq.com>
2020-05-24 02:33:14 +0800

    steiner restreint

--- projetCPAfinal/src/algorithms/DefaultTeam.java
+++ projetCPAfinal/src/algorithms/DefaultTeam.java
@@ -89,18 +89,59 @@ public class DefaultTeam {
     }
 
     public Tree2D calculSteinerBudget(ArrayList<Point> points, int edgeThreshold, ArrayList<Point> hitPoints) {
-        // REMOVE >>>>>
-        Tree2D leafX = new Tree2D(new Point(700, 400), new ArrayList<Tree2D>());
-        Tree2D leafY = new Tree2D(new Point(700, 500), new ArrayList<Tree2D>());
-        Tree2D leafZ = new Tree2D(new Point(800, 450), new ArrayList<Tree2D>());
-        ArrayList<Tree2D> subTrees = new ArrayList<Tree2D>();
-        subTrees.add(leafX);
-        subTrees.add(leafY);
-        subTrees.add(leafZ);
-        Tree2D steinerTree = new Tree2D(new Point(750, 450), subTrees);
-        System.out.printf("root(%f,%f)", steinerTree.getRoot().getX(), steinerTree.getRoot().getY());
-        // <<<<< REMOVE
+        /**
+         * L'algo floyd remplit le tableau distance et paths, la complexité est O(n3).
+         * dis[u][v] est la plus petite distance entre Point u et Point v
+         * paths[u][v] est le point de successeur dans le plus court chemin de u à v.
+         * idents stocke les identifiants de point. 
+         */
+        int[][] paths = new int[points.size()][points.size()];
+        double[][] dis = new double[points.size()][points.size()];
+        Floyd.floyd(paths, dis, points, edgeThreshold);
 
-        return steinerTree;
+        HashMap<Point, Integer> idents = new HashMap<Point, Integer>();
+        for (int i = 0; i < points.size(); i++) {
+            idents.put(points.get(i), i);
+        }
+        System.out.printf("1st kruskal, hitPoint=%d\n", hitPoints.size());
+
+        /**
+         * L'algo kruskal rend l'arbre couvrant minimal de S (hitPoitns).
+         */
+        UFSet ufs = new UFSet(points);
+        ArrayList<Edge> spanning = Kruskal.kruskal(hitPoints, ufs, idents, dis, paths);
+        System.out.printf("spanning size=%d\n", spanning.size());
+
+        /** 
+         * Dès qu'on a obtenu l'arbre couvrant minimal de S,
+         * on remplace les arêtes de l'arbre couvrant de S par le plus court chemin de G.
+         * En plus ajouter les points qui sont sur le chemin dans la nouvelle liste de point H.
+        */
+        HashSet<Point> setPoint = new HashSet<Point>();
+        for (Point p : hitPoints) {
+            setPoint.add(p);
+        }
+        for (Edge e : spanning) {
+            int u = idents.get(e.u);
+            int v = idents.get(e.v);
+            // ajouter tous les sommets entre u et v
+            while (paths[u][v] != v) {
+                int k = paths[u][v];
+                if (!setPoint.contains(points.get(k))) {
+                    setPoint.add(points.get(k));
+                }
+                u = k;
+            }
+        }
+
+        /**
+         * La deuxième fois de l'algo kruskal permet de construire l'arbre couvrant T' depuis H.
+        */
+        ArrayList<Point> allPoint = new ArrayList<Point>(setPoint);
+        System.out.printf("2nd kruskal, points=%d\n", allPoint.size());
+        ufs = new UFSet(allPoint);
+        spanning = Kruskal.kruskal(allPoint, ufs, idents, dis, paths);
+        System.out.printf("spanning.size=%d\n", spanning.size());
+        return edgesToTree(spanning, spanning.get(0).u);
     }
 }
